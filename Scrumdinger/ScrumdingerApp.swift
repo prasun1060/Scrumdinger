24f02e271125c6a58083f9bcb68218304c350d53
Prasun Bhattacharyya <bhattacharyyaprasun47@gmail.com>
2022-09-17 10:55:00 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Prasun Bhattacharyya on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
